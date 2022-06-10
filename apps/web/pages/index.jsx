65e7a94f1ebb184c48a5c12c1bba790102272dfb
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-10 12:39:50 -0400

    chore(web): :hammer: rename to jsx

--- apps/web/pages/index.jsx
+++ apps/web/pages/index.jsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from "ui/nextui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <Button>Boop!</Button>
     </div>
   );
 }
