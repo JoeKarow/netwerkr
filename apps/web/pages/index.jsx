a80b0d5c1d18e0a7126cd8b43392440873a84d18
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-12 00:06:42 -0400

    feat(ui): :sparkles: create auth status component for testing

--- apps/web/pages/index.jsx
+++ apps/web/pages/index.jsx
@@ -1,5 +1,6 @@
 import { Layout } from '@joekarow/netwerkr-ui/layout/web'
 import { HeroImageRight, FeaturesGrid } from '@joekarow/netwerkr-ui/section'
+import { AuthSessionStatus } from '@joekarow/netwerkr-ui/components'
 import useSWR from 'swr'
 import axios from 'axios'
 import * as heroBackground from '../public/img/chris-j-davis-7ezFz2Hxd40-unsplash.jpg'
@@ -9,6 +10,7 @@ export default function Main ( props ) {
   const { title, description, featureDetails } = features
   return (
     <>
+      <AuthSessionStatus />
       <HeroImageRight image={ heroBackground } />
       <FeaturesGrid
         title={ title }
---
+++ packages/ui/components/index.js
@@ -0,0 +1,2 @@
+// @index('./**/*.{jsx,tsx}', f => `export { default as ${f.name} } from '${f.path}'`)
+export { default as AuthSessionStatus } from './AuthSessionStatus'
