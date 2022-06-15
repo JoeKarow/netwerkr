e14be120a9f3373348ad7557e2e3fa0d6fbc2324
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-15 01:12:28 -0400

    fix(web,ui): :bug: useContext hook error on initial page load
    shuffled around order of the auth & mantine provider. removed the nav api fetch and just hardcoded
    it for now.

---
+++ packages/ui/layout/navLinks.js
@@ -0,0 +1,28 @@
+export const links = [
+    {
+        link: "/",
+        label: "Home"
+    },
+    {
+        link: "/placeholder",
+        label: "Placeholder list",
+        links: [
+            {
+                link: "/itemone",
+                label: "list item one"
+            },
+            {
+                link: "/itemtwo",
+                label: "list item two"
+            },
+            {
+                link: "/itemthree",
+                label: "list item three"
+            },
+            {
+                link: "/itemfour",
+                label: "list item four"
+            }
+        ]
+    }
+]
--- packages/ui/layout/web.jsx
+++ packages/ui/layout/web.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import { HeaderAction } from '../section'
-import useSWR from 'swr'
+import { links } from './navLinks'
 import { AppShell, Container } from '@mantine/core'
 
 const fetcher = ( url ) => fetch( url ).then( ( res ) => res.json() )
-export const Layout = ( { children, nav } ) => {
-    const { data, error } = useSWR( '/api/navigation', fetcher )
-    if ( error ) {
-        return <>Failed to load links</>
-    }
-    if ( !data ) return <>Loading...</>
+export const Layout = ( { children } ) => {
+
 
     return (
         <AppShell
-            header={ <HeaderAction links={ data.links } /> }
+            header={ <HeaderAction links={ links } /> }
         >
             <Container size='xl'>
                 { children }
