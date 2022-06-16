b1d0bfe90e90e83848c56c8084910177468c0077
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-16 00:46:45 -0400

    feat(web,ui): :sparkles: added login modal, Auth0
    Added Auth0 just so I could render more than one button for testing

--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -2,39 +2,49 @@ import Head from 'next/head';
 import { MantineProvider } from '@joekarow/netwerkr-ui/mantine/core';
 import { SessionProvider } from 'next-auth/react'
 
-const LayoutProvider = ( { Component, pageProps: {...pageProps} } ) => {
+const LayoutProvider = ( { Component, pageProps: { ...pageProps } } ) => {
     const getLayout = Component.getLayout || ( ( page ) => page )
 
-    return getLayout((
-            <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={ {
-                    /** Put your mantine theme override here */
-                    colorScheme: 'light'
-                } }
-            >
-        <Component {...pageProps}  />
-            </MantineProvider>
-        
+    return getLayout( (
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={ {
+                /** Put your mantine theme override here */
+                colorScheme: 'light'
+            } }
+        >
+            <Component { ...pageProps } />
+        </MantineProvider>
+
     )
     )
 }
 
 
-export default function App (props) {
-
-    const {session, ...pageProps} = props
+export default function App ( { Component, pageProps: { session, ...pageProps } } ) {
+    // console.log( 'app props', pageProps )
+    // console.log( 'session', session )
+    // const { session, ...pageProps } = props
+    const getLayout = Component.getLayout || ( ( page ) => page )
     return (
         <>
-            <Head>
-                <title>netwerkr</title>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-            </Head>
-
-                <SessionProvider session={ session }>
-                    <LayoutProvider {...pageProps} />
-                </SessionProvider>
+            <SessionProvider session={ session }>
+                <Head>
+                    <title>netwerkr</title>
+                    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                </Head>
+                <MantineProvider
+                    withGlobalStyles
+                    withNormalizeCSS
+                    theme={ {
+                        /** Put your mantine theme override here */
+                        colorScheme: 'light'
+                    } }
+                >
+                    { getLayout( <Component { ...pageProps } /> ) }
+                </MantineProvider>
+            </SessionProvider>
 
         </>
     );
--- packages/ui/components/index.js
+++ packages/ui/components/index.js
@@ -1,2 +1,3 @@
 // @index('./**/*.{jsx,tsx}', f => `export { default as ${f.name} } from '${f.path}'`)
 export { default as AuthSessionStatus } from './AuthSessionStatus'
+export { default as LoginProvider } from './LoginProvider'
