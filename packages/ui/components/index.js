30a0fbc94eff04b26ac29dfe24cf63c0295d8840
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-18 18:26:26 -0400

    chore(ui): :hammer: module indexing

--- packages/ui/components/index.js
+++
@@ -1,3 +0,0 @@
-// @index('./**/*.{jsx,tsx}', f => `export { default as ${f.name} } from '${f.path}'`)
-export { default as AuthSessionStatus } from './AuthSessionStatus'
-export { default as LoginProvider } from './LoginProvider'
---
+++ packages/ui/mockdata/index.js
@@ -0,0 +1,4 @@
+// @index('./**/*.{js*,(ts* && !d.*)}', f => `export * from '${f.path}'`)
+export * from './contactTable'
+export * from './navbarNested'
+export * from './user'
