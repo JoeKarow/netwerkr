872fb348de7228b07b0f9308f19c9b2e95d5e253
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-18 18:26:26 -0400

    chore(ui): :hammer: module indexing

--- packages/ui/section/index.js
+++ packages/ui/section/index.js
@@ -1,4 +1,6 @@
-// @index('./**/*.{jsx,tsx}', f => `export { default as ${f.name} } from '${f.path}'`)
-export { FeaturesGrid } from './FeaturesGrid'
-export { default as HeaderAction } from './HeaderAction'
-export { default as HeroImageRight } from './HeroImageRight'
+// @index('./**/*.{jsx,tsx}', f => `export * from '${f.path}'`)
+export * from './FeaturesGrid'
+export * from './HeaderAction'
+export * from './HeaderUser'
+export * from './HeroImageRight'
+export * from './NavbarNested'
