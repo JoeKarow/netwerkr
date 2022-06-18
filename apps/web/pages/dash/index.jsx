f930537cd13a176d32cb0c1161b0be22a21c3e24
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-18 18:30:18 -0400

    feat(web,ui): :sparkles: create initial user dash layout

---
+++ apps/web/pages/dash/index.jsx
@@ -0,0 +1,24 @@
+import { DashLayout } from '@joekarow/netwerkr-ui/layout'
+import { ContactTable } from '@joekarow/netwerkr-ui/components'
+import { contactTable } from '@joekarow/netwerkr-ui/mockdata'
+
+
+const Dash = () => {
+    return (
+        <ContactTable data={ contactTable } />
+    )
+}
+
+
+Dash.getLayout = ( page ) => {
+    return (
+        <DashLayout>
+            { page }
+        </DashLayout>
+    )
+}
+
+
+
+
+export default Dash
