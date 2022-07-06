144989258b3eff996f1f8cd4c5bf1ed11f677eb8
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-06 11:05:05 -0400

    refactor(ui): :recycle: authSessionStatus to devtools

--- src/components/devtools.tsx
+++ src/components/devtools.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import { Container, Text } from '@mantine/core'
 import { useSession } from 'next-auth/react'
+
+export const JsonView = (data: any) => {
+	return (
+		<Container fluid>
+			<Text sx={{ whiteSpace: 'pre-wrap' }}>
+				{JSON.stringify(data, null, 2)}
+			</Text>
+		</Container>
+	)
+}
 
 export const AuthSessionStatus = () => {
 	const { data: session, status } = useSession()
