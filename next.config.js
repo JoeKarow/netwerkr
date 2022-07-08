b23cd83d742104783b72a8c4bbe9956f6e879319
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-08 11:22:26 -0400

    chore(conf): :hammer: change import paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+const { env } = require('./src/server/env')
+
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+
+function getConfig(config) {
+	return config
+}
+
 const nextConfig = {
 	reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+module.exports = getConfig({
+	/**
+	 * Dynamic configuration available for the browser and server.
+	 * Note: requires `ssr: true` or a `getInitialProps` in `_app.tsx`
+	 * @link https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
+	 */
+	publicRuntimeConfig: {
+		NODE_ENV: env.NODE_ENV,
+	},
+})
