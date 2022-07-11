e9bc396f9fe784520b9c7ba8dcb16101819225ce
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-11 15:36:53 -0400

    refactor(app): :recycle: page for logged in user only

--- src/pages/dash/profile/index.tsx
+++ src/pages/dash/profile/index.tsx
@@ -1,29 +1,56 @@
-import React from 'react'
-import { useSession } from 'next-auth/react'
+import React, { ReactNode } from 'react'
 import { DashLayout } from '~/layout'
 import { ProfileGrid } from '~/section'
 import { JsonView } from '~/components/devtools'
-// import { getProfile } from "@/db/getProfile";
-import type { NextPageWithLayout } from 'next'
+import { trpc } from '~/utils/trpc'
+import { useSession } from 'next-auth/react'
+// import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+// import { createSSGHelpers } from '@trpc/react/ssg'
+// import { appRouter, createContext } from '~/pages/api/trpc/[trpc]'
+// import superjson from 'superjson'
 
-const Profile: NextPageWithLayout = props => {
-	const { data } = props
-	// const { data: session } = useSession()
+// export const getServerSideProps = async (
+// 	context: GetServerSidePropsContext<{ uid: string }>
+// ) => {
+// 	try {
+// 		// const ctx = await createContext(context)
+// 		const ssg = await createSSGHelpers({
+// 			router: appRouter,
+// 			ctx: await createContext(),
+// 			transformer: superjson,
+// 		})
+
+// 		const id = context.params?.uid as string
+// 		await ssg.fetchQuery('contact.getAllContacts', { id })
+
+// 		return {
+// 			props: {
+// 				trpcState: ssg.dehydrate(),
+// 				id,
+// 			},
+// 			// revalidate: 1,
+// 		}
+// 	} catch (error) {
+// 		console.error(error)
+// 	}
+// }
+
+export default function Profile() {
+	const { data: session } = useSession()
+	const id = session?.user.id || ''
+	const contactQuery = trpc.proxy.user.getById.useQuery({ id })
+
+	const { data } = contactQuery
 	// console.log( data )
 	return (
 		<>
 			<ProfileGrid />
-			<JsonView data={props} />
+			<JsonView data={data} />
 		</>
 	)
 }
-Profile.getLayout = page => {
+Profile.getLayout = (page: ReactNode) => {
 	return <DashLayout>{page}</DashLayout>
 }
 
-// export const getServerSideProps = async () => {
-//     const session = await unstable_getServerSession()
-//     console.log( session )
-//     return { props: { session } }
-// }
-export default Profile
+//http://localhost:3000/dash/profile/62ba7634dc0748278a9ce027
