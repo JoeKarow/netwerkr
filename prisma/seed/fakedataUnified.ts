71caea153c6703db03fa6f7819f8cb072e5f3a46
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-13 22:15:09 -0400

    style(db): :lipstick: clean up imports

--- prisma/seed/fakedataUnified.ts
+++ prisma/seed/fakedataUnified.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-import { Prisma, PrismaClient } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 
 // SocialMediaCreateWithoutContactInput
 
